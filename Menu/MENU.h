66525f731fc1b3421c7bd64f4b53662894a91f60
alejohk13-svg <alejohk13@gmail.com>
2026-06-09 14:39:05 -0300

    Entrega de TP3.
    * Entrega de TP3. Se utilizo nueva rama por problema a la hora de realizar el PR
    
    * Correcciones solicitadas por el docente realizadas. Aparte de esto se agrego global.c y global.h, se acomodo y limpio el codigo
    
    * Se acomodo menu.c

---
+++ Menu/MENU.h
@@ -0,0 +1,18 @@
+#ifndef __MENU_H
+#define __MENU_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stdint.h"
+
+void MENU_Init(void);
+void MENU_MostrarOpciones(void);
+void MENU_Update(char tecla);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif
